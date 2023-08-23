a81aeb5493c37328ddb42c92319c9290d527ab2e
Artem Ulianytskyi <32933673+Ulianytskyi@users.noreply.github.com>
2023-08-23 10:29:22 +0300

    Commit 2
    Actions and Events

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const gameField = document.getElementById('game-field');
 
+const eventDisplay = document.getElementById('event-display');
+const eventDisplayClose = document.getElementById('event-display-close');
+const eventImg1 = document.getElementById('event-img1');
+const eventImg2 = document.getElementById('event-img2');
+const eventText = document.getElementById('event-text');
+
+const actionsPanel = document.querySelector('.actions-panel');
+const btnAction = document.getElementById('btn-action');
+const btnLook = document.getElementById('btn-look');
+const btnUse = document.getElementById('btn-use');
+const btnTalk = document.getElementById('btn-talk');
+const btnWalkOut = document.getElementById('btn-walk-out');
 
 let objects = ['👨🏻','🧔🏻‍','👨🏼','🌳','🌳','🌲','🌲','🌾','🌾','🍄','🍎','🏠'];
-
 let field = [];
 
 let rows = 7;
 let cols = 7;
 let count = 0;
-let currTile, nextTile;
-let tempTile;
-let player;
+let currTile, nextTile, tempTile, player;
+let isActive = false;
+let isWalkable = true;
 
 let gameFieldWidth = 54 * cols;
 gameField.style.width = gameFieldWidth + 'px';
 
+// generate game field ----------------------------------------------------------
 
 function generateField() {
 	for (let i = 0; i < rows; i++) {
@@ -47,7 +59,6 @@ function randomObject() {
 }
 
 function playerSpawn() {
-
 	tempTile = field[0][0].innerText;
 
 	field[0][0].innerText = objects[0];
@@ -61,20 +72,18 @@ function playerSpawn() {
 }
 
 function npcSpawn() {
-
 	let tempRow = Math.floor((Math.random() * rows));
 	let tempCol = Math.floor((Math.random() * cols));
 	let tempIndexOfNpc = Math.floor((Math.random() * 2) + 1);
-
-	console.log(tempRow, tempCol, tempIndexOfNpc);
-
 	let isPermit = tempRow != 0 && tempRow != rows - 1 && tempCol != 0 && tempCol != cols - 1;
-	console.log(isPermit, tempRow, tempCol, tempIndexOfNpc);
-
 	if (isPermit) {
 		field[tempRow][tempCol].innerText = objects[tempIndexOfNpc];
 	}
 }
+
+generateField();
+
+// take coordinates --------------------------------------------------------------------
 
 function handleMouseDown() {
 	currTile = document.getElementById('player-object');
@@ -97,10 +106,7 @@ function checkMove() {
 	let moveDown = col1 == col2 && row2 == row1 + 1;
 
 	let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
-
-	console.log(isAdjacent);
-
-	if (isAdjacent) {
+	if (isAdjacent && isWalkable) {
 		let tempObj1 = field[row1][col1].innerText;
 		let tempObj2 = field[row2][col2].innerText;
 		
@@ -110,9 +116,72 @@ function checkMove() {
 		field[row1][col1].classList.remove("marker");
 		field[row2][col2].innerText = tempObj1;
 		field[row2][col2].id = "player-object";
-		field[row2][col2].classList.add("marker");
+		field[row2][col2].classList.add("marker");	
 	}
+	takeCurrentTileContent(row1, col1);
 }
 
-generateField();
+function takeCurrentTileContent(row, col) {
+	return tempTile;
+}
 
+// action buttons ----------------------------------------------------------
+
+btnAction.addEventListener('click', function() {
+	if (!isActive) {
+		actionsPanel.classList.remove('hide');
+		
+		btnAction.textContent = "Leave";
+		isWalkable = false;
+		isActive = true;
+	} else {
+		actionsPanel.classList.add('hide');
+		btnAction.textContent = "Action";
+		isWalkable = true;
+		isActive = false;
+	}
+});
+
+btnLook.addEventListener('click', function() {
+	if (isActive) {
+		let tempValue = takeCurrentTileContent();
+		eventDisplay.classList.remove('hide');
+		eventImg1.innerHTML = objects[0];
+		eventImg2.innerHTML = tempValue;
+		eventText.innerHTML = `You see ${tempValue}.`;
+	}
+});
+
+btnUse.addEventListener('click', function() {
+	if (isActive) {
+		let tempValue = takeCurrentTileContent();
+		eventDisplay.classList.remove('hide');
+		eventImg1.innerHTML = objects[0];
+		eventImg2.innerHTML = tempValue;
+		eventText.innerHTML = `You can touch this ${tempValue}.`;
+	}
+});
+
+btnTalk.addEventListener('click', function() {
+	if (isActive) {
+		let tempValue = takeCurrentTileContent();
+		eventDisplay.classList.remove('hide');
+		eventImg1.innerHTML = objects[0];
+		eventImg2.innerHTML = tempValue;
+		eventText.innerHTML = `You say this ${tempValue} hello.`;
+	}
+});
+
+btnWalkOut.addEventListener('click', function() {
+	if (isActive) {
+		let tempValue = takeCurrentTileContent();
+		eventDisplay.classList.remove('hide');
+		eventImg1.innerHTML = objects[0];
+		eventImg2.innerHTML = tempValue;
+		eventText.innerHTML = `You can't walk away from this zone.`;
+	}
+});
+
+eventDisplayClose.addEventListener('click', function() {
+	eventDisplay.classList.add('hide');
+})
